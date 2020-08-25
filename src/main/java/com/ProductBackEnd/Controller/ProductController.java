cdc871ea36b5f74abb7dc4ceb0ea527e78e0ea64
Tesfahiwet Ameash <tesfahiwetameash@Tesfahiwets-MacBook-Pro.local>
2020-08-24 23:35:33 -0500

    test  CrossOrigin

--- src/main/java/com/ProductBackEnd/Controller/ProductController.java
+++ src/main/java/com/ProductBackEnd/Controller/ProductController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins="http://localhost:4200")
+//@CrossOrigin(origins="http://localhost:4200")
 //@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class ProductController {
